6213282e70b7e9becb07b68430c662b518dbb782
fanjieqi <fanjieqi@nibirutech.com>
2017-04-07 19:00:00 +0800

    add the basic code to query

--- lib/claw_druid.rb
+++ lib/claw_druid.rb
@@ -1,2 +1,12 @@
+require 'httparty'
+
 class ClawDruid
+  def initialize(url = "", source = "")
+    @url    = url
+    @source = source
+  end
+
+  def query(params = {})
+    HTTParty.post(@url, body: params.to_json, headers: { 'Content-Type' => 'application/json' })
+  end
 end
