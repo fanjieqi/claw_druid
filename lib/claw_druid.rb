2e5029e0715332e0efeb955260d4984a7b57ab7c
fanjieqi <fanjieqi@nibirutech.com>
2017-04-12 11:19:08 +0800

    try to add the page method

--- lib/claw_druid.rb
+++ lib/claw_druid.rb
@@ -3,17 +3,23 @@ require 'json'
 require 'awesome_print'
 
 class ClawDruid
+  THRESHOLD = 10
+
   def initialize(url = "", source = "")
     @url    = url
-    @params = {dataSource: source, granularity: "day", queryType: "groupBy"}
+    @params = {dataSource: source, granularity: "day", queryType: "select"}
+
+    # The page_identifiers of every query, the key is the params.hash of the query, the value is a hash like {1 => identifiers1, 2 => identifiers2}
+    @paging_identifiers = {}
+    @current_page = nil
   end
 
   def group(*dimensions)
     @params[:queryType]  = "groupBy"
     if dimensions.count == 1
-      @params[:dimension] = dimensions[0]
+      @params[:dimension] = dimensions[0].to_s.strip
     else
-      @params[:dimensions] = dimensions.map(&:to_s)
+      @params[:dimensions] = dimensions.map(&:to_s).map(&:strip)
     end
     self
   end
@@ -86,14 +92,40 @@ class ClawDruid
     self
   end
 
+  def page(page_count)
+    current = @params.hash
+    @paging_identifiers[current] ||= {}
+    if page_count == 1
+      @params[:pagingSpec] = {pagingIdentifiers: {}, threshold: THRESHOLD}
+    elsif @paging_identifiers[current][page_count - 1]
+      @params[:pagingSpec] = {pagingIdentifiers: @paging_identifiers[current][page_count - 1], threshold: THRESHOLD}
+    else
+      last_threshold = THRESHOLD * (page_count - 1)
+      last_page = query(@params.merge(pagingSpec: {pagingIdentifiers:  {}, threshold: last_threshold}))
+
+      last_identifiers = last_page["result"]["pagingIdentifiers"]
+      last_identifiers.each{|key, value| last_identifiers[key] += 1}
+
+      @paging_identifiers[current][page_count - 1] = last_identifiers
+      @params[:pagingSpec] = {pagingIdentifiers: last_identifiers, threshold: THRESHOLD}
+      @current_page = page_count
+    end
+    self
+  end
+
   def query(params = @params)
     HTTParty.post(@url, body: params.to_json, headers: { 'Content-Type' => 'application/json' })
   end
 
   def inspect
-    # ap @params
     puts @params.to_json
-    HTTParty.post(@url, body: @params.to_json, headers: { 'Content-Type' => 'application/json' })
+    result = HTTParty.post(@url, body: @params.to_json, headers: { 'Content-Type' => 'application/json' })
+    if result[:pagingIdentifiers]
+      @params.delete(:pagingSpec)
+      current = @params.hash
+      @paging_identifiers[current][@current_page] = result[:pagingIdentifiers]
+    end
+    result
   end
 
 end
